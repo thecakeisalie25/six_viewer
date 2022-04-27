43e9d50fde0fada1ebe2adcc8328af1603e71b23
Larson Ashcroft <larsona@outlook.com>
2022-04-26 23:00:26 -0500

    Switch colors

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ fn main() -> anyhow::Result<()> {
     for line in BufReader::new(File::open("./output.txt")?).lines() {
         for c in line?.chars() {
             match c.to_ascii_uppercase() {
-                'B' => print!("{}", nu_ansi_term::Color::Yellow.paint("B")),
-                'W' => print!("{}", nu_ansi_term::Color::LightYellow.paint("W")),
+                'B' => print!("{}", nu_ansi_term::Color::LightYellow.paint("B")),
+                'W' => print!("{}", nu_ansi_term::Color::Yellow.paint("W")),
                 'T' => print!("{}", nu_ansi_term::Color::Red.paint("T")),
                 '_' => print!("{}", nu_ansi_term::Color::White.paint("_")),
                 other => print!("{}", nu_ansi_term::Color::default().paint::<String, _>(other.into())),
